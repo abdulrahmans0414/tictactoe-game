3ccb8425078592386a83246f870be5b5b4e89234
Abdul_Rahman <iamarsiddiqui@gmail.com>
2023-08-21 00:49:36 +0530

    installed sass, create bord and square components barebones

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import Square from "./components/Square";
-import "./App.css";
+import "./styles.scss";
+import Board from "./components/Board";
+
 
 function App() {
   return (
-    <div>
-      <h1>Title</h1>
-
-      <Square>
-        <div>hello there</div>
-        <h5>this is title</h5>
-      </Square>
-      <Square value="something" />
-      <Square value="hi" />
-      <Square value="5" />
+    <div className="app">
+     <Board />
     </div>
   );
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,12 +1,13 @@
 
-const Square = ({value, children}) =>{
-    console.log(children)
+const Square = ({value}) =>{
+
     return (
-        <div>
-            {value}
-            <h5>Children will be render below</h5>
-            {children}
-        </div>
+        
+            <button type="button" className="square">
+                {value}
+            </button>
+            
+       
     )
 }
 
