e25be1f63930e8144e38a283742393852076ddbe
Abdul_Rahman <iamarsiddiqui@gmail.com>
2023-08-22 16:47:39 +0530

    learn about event and useState hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./styles.scss";
 import Board from "./components/Board";
 
 
 function App() {
+
+  const [counter, setCounter]= useState(1);
+
+
+  const onBtnClick = (event)=>{
+    console.log("hello!",event);
+
+
+    setCounter(counter+1)
+  }
+
   return (
     <div className="app">
-     <Board />
+     <div>
+      <button onClick={onBtnClick}>Click me please</button>
+      <div>{counter}</div>
+     </div>
     </div>
   );
 }
